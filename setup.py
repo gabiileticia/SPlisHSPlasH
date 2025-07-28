1b95bec2c90f8f27e0c82df190fbdecdd6bfc8e9
Gabi <gabriela.leticia.souza@gmail.com>
2025-07-28 14:47:21 +0200

    Reverting python changes to test on cluster

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ class CMakeBuild(build_ext):
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-        cfg = 'Release'
+        cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
         # Add cmake command line arguments
